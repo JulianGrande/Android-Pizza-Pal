ef2bf811982b83894cca17a97613764aee6f5abe
Julian Grande <juliangrande11@gmail.com>
2023-12-06 20:00:10 -0500

    starting specialty

--- app/src/main/java/com/beulah/cs213p5/SpecialtyPizzaActivity.java
+++ app/src/main/java/com/beulah/cs213p5/SpecialtyPizzaActivity.java
@@ -1,7 +1,19 @@
 package com.beulah.cs213p5;
 
-public class SpecialtyPizzaActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpecialtyPizzaActivity extends AppCompatActivity {
 
     //Speciality pizza stuff here
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specialty);
+        Cashier cashier = Cashier.Cashier();
+    }
+
 
 }
