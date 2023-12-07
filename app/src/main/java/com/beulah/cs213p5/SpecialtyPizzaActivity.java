55874628dcd5206244735a7052f0822ec0aa208d
GoldenPotato14 <68200448+GoldenPotato14@users.noreply.github.com>
2023-12-06 20:50:34 -0500

    hello kitty

--- app/src/main/java/com/beulah/cs213p5/SpecialtyPizzaActivity.java
+++ app/src/main/java/com/beulah/cs213p5/SpecialtyPizzaActivity.java
@@ -1,11 +1,16 @@
 package com.beulah.cs213p5;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SpecialtyPizzaActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private ArrayAdapter<String> adapter;
 
     //Speciality pizza stuff here
     @Override
@@ -13,6 +18,24 @@ public class SpecialtyPizzaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specialty);
         Cashier cashier = Cashier.Cashier();
+
+        String[] specialty_pizzas = {"Deluxe", "Meatzza", "Pepperoni", "Seafood",
+                                        "Supreme"};
+        adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.
+                                                support_simple_spinner_dropdown_item,
+                                                specialty_pizzas);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int postion, long id){
+        String selectedPizza = spinner.getSelectedItem().toString;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent){
+        //leave blank
     }
 
 
