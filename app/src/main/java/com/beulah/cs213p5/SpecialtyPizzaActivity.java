50fd55cafe8f1a509c4aad9609976a084c4bbe51
Julian Grande <juliangrande11@gmail.com>
2023-12-06 21:30:24 -0500

    working on specialty, using new branch

--- app/src/main/java/com/beulah/cs213p5/SpecialtyPizzaActivity.java
+++ app/src/main/java/com/beulah/cs213p5/SpecialtyPizzaActivity.java
@@ -1,42 +1,22 @@
 package com.beulah.cs213p5;
 
 import android.os.Bundle;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SpecialtyPizzaActivity extends AppCompatActivity {
 
-    private Spinner spinner;
-    private ArrayAdapter<String> adapter;
+    private RecyclerView recycler;
+    private Pizza pizza;
+    private PizzaMaker pizzaMaker;
 
     //Speciality pizza stuff here
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specialty);
         Cashier cashier = Cashier.Cashier();
-
-        String[] specialty_pizzas = {"Deluxe", "Meatzza", "Pepperoni", "Seafood",
-                                        "Supreme"};
-        adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.
-                                                support_simple_spinner_dropdown_item,
-                                                specialty_pizzas);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
     }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int postion, long id){
-        String selectedPizza = spinner.getSelectedItem().toString;
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent){
-        //leave blank
-    }
-
 
 }
